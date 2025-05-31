5d2ce88fd5ba33100b4639c84325293f20165dba
Kees Jan Koster <kjkoster@kjkoster.org>
2025-05-31 21:05:32 +0200

    Add button to enter maintenance mode. Break out semaphore into its own module.

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,20 @@
 // https://dev.to/theembeddedrustacean/embedded-rust-embassy-gpio-button-controlled-blinking-3ee6
 // https://www.youtube.com/watch?v=dab_vzVDr_M
 
+use core::sync::atomic::{AtomicBool, Ordering};
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Speed};
-use embassy_sync::{
-    blocking_mutex::raw::ThreadModeRawMutex,
-    semaphore::{FairSemaphore, Semaphore},
+use embassy_stm32::{
+    exti::{AnyChannel, Channel, ExtiInput},
+    gpio::{AnyPin, Level, Output, Pin, Pull, Speed},
 };
 use embassy_time::Timer;
 use panic_halt as _;
 
 mod trafficlight;
 use trafficlight::TrafficLight;
+use trafficlight::semaphore::{NUM_TRAFFICLIGHTS, acquire_permit, release_permit};
 
-const NUM_TRAFFICLIGHTS: usize = 2;
-
-type CrossingSemaphore = FairSemaphore<ThreadModeRawMutex, NUM_TRAFFICLIGHTS>;
-static CROSSING_SEMAPHORE: CrossingSemaphore = CrossingSemaphore::new(1);
+static MAINTENANCE_MODE: AtomicBool = AtomicBool::new(false);
 
 // Deal with active-high or active-low, so that the state machine can just use
 // easy to understand `true` for on logic.
@@ -29,31 +27,43 @@ fn light(led: &mut Output, on: bool) {
 
 #[embassy_executor::task(pool_size = NUM_TRAFFICLIGHTS)]
 async fn trafficlight_task(pin_red: AnyPin, pin_amber: AnyPin, pin_green: AnyPin) {
-    let mut red = Output::new(pin_red, Level::Low, Speed::Low);
+    let mut red = Output::new(pin_red, Level::High, Speed::Low);
     let mut amber = Output::new(pin_amber, Level::Low, Speed::Low);
     let mut green = Output::new(pin_green, Level::Low, Speed::Low);
 
     let mut trafficlight = TrafficLight::new();
-    let mut holds_permit = false;
     loop {
+        Timer::after_millis(trafficlight.phase_time_seconds() * 1000).await;
+
+        trafficlight
+            .go_to_next_phase(MAINTENANCE_MODE.load(Ordering::Relaxed))
+            .await;
+
         light(&mut red, trafficlight.red());
         light(&mut amber, trafficlight.amber());
         light(&mut green, trafficlight.green());
+    }
+}
 
-        Timer::after_millis(trafficlight.phase_time_seconds() * 1000).await;
+#[embassy_executor::task]
+async fn maintenance_button_task(pin_button: AnyPin, interrupt_channel: AnyChannel) {
+    let mut button = ExtiInput::new(pin_button, interrupt_channel, Pull::Up);
 
-        trafficlight.to_next_phase();
-        match (holds_permit, trafficlight.needs_permit()) {
-            (false, true) => {
-                CROSSING_SEMAPHORE.acquire(1).await.unwrap().disarm();
-                holds_permit = true;
-            }
-            (true, false) => {
-                CROSSING_SEMAPHORE.release(1);
-                holds_permit = false;
-            }
-            (false, false) | (true, true) => {}
+    loop {
+        button.wait_for_low().await;
+
+        // XXX no: both lights need to be in `Phase::Stop` before I can enable maintenance mode.
+        //
+        MAINTENANCE_MODE.fetch_not(Ordering::Relaxed);
+        if MAINTENANCE_MODE.load(Ordering::Relaxed) {
+            acquire_permit().await;
+        } else {
+            release_permit();
         }
+
+        // debounce....
+        Timer::after_millis(200).await;
+        button.wait_for_high().await;
     }
 }
 
@@ -75,14 +85,28 @@ async fn main(spawner: Spawner) {
             peripherals.PE0.degrade(),
         ))
         .unwrap();
+    spawner
+        .spawn(maintenance_button_task(
+            peripherals.PE11.degrade(),
+            peripherals.EXTI11.degrade(),
+        ))
+        .unwrap();
 
     // Show and help count seconds by flashing the on-board LED roughly once
     // every second.
     let mut led4 = Output::new(peripherals.PE12, Level::Low, Speed::Low);
     loop {
         led4.set_low();
-        Timer::after_millis(15).await;
+        if MAINTENANCE_MODE.load(Ordering::Relaxed) {
+            Timer::after_millis(500).await;
+        } else {
+            Timer::after_millis(15).await;
+        }
         led4.set_high();
-        Timer::after_millis(985).await;
+        if MAINTENANCE_MODE.load(Ordering::Relaxed) {
+            Timer::after_millis(500).await;
+        } else {
+            Timer::after_millis(985).await;
+        }
     }
 }
