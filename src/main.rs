6a70d3f544c171ee483ef16d424c07956ac2e109
Kees Jan Koster <kjkoster@kjkoster.org>
2025-06-12 07:41:19 +0200

    Refactor normal and flash mode to be a state enum, so that I can have more states in the future.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@
 mod io;
 use io::{CHANNEL_CAPACITY, Leg, Rag, io_task};
 
-use core::sync::atomic::{AtomicBool, Ordering};
+use atomic_enum::atomic_enum;
+use core::sync::atomic::Ordering;
 use embassy_executor::Spawner;
 use embassy_sync::{
     blocking_mutex::raw::ThreadModeRawMutex,
@@ -27,9 +28,24 @@ const NUM_FLASH_BUTTON_TASKS: usize = 1;
 const NUM_TASKS: usize = NUM_NORMAL_MODE_TASKS + NUM_FLASH_MODE_TASKS + NUM_FLASH_BUTTON_TASKS;
 
 type CrossingSemaphore = FairSemaphore<ThreadModeRawMutex, NUM_TASKS>;
-static NORMAL_MODE_SEMAPHORE: CrossingSemaphore = CrossingSemaphore::new(1);
+static NORMAL_MODE_SEMAPHORE: CrossingSemaphore = CrossingSemaphore::new(0);
 static FLASH_MODE_SEMAPHORE: CrossingSemaphore = CrossingSemaphore::new(0);
-static IN_FLASH_MODE: AtomicBool = AtomicBool::new(false);
+
+#[atomic_enum]
+#[derive(PartialEq, Eq)]
+enum SystemMode {
+    Normal,
+    Flash,
+}
+static SYSTEM_MODE: AtomicSystemMode = AtomicSystemMode::new(SystemMode::Normal);
+impl AtomicSystemMode {
+    fn to_next_mode(&self) {
+        match self.load(Ordering::Relaxed) {
+            SystemMode::Normal => self.store(SystemMode::Flash, Ordering::Relaxed),
+            SystemMode::Flash => self.store(SystemMode::Normal, Ordering::Relaxed),
+        }
+    }
+}
 
 static RAGS: Channel<ThreadModeRawMutex, Rag, CHANNEL_CAPACITY> = Channel::new();
 static BLINKY: Channel<ThreadModeRawMutex, bool, CHANNEL_CAPACITY> = Channel::new();
@@ -75,7 +91,7 @@ async fn normal_mode_task(
 #[embassy_executor::task(pool_size = NUM_FLASH_MODE_TASKS)]
 async fn flash_mode_task(
     semaphore: &'static CrossingSemaphore,
-    in_flash_mode: &'static AtomicBool,
+    system_mode: &'static AtomicSystemMode,
     rags: Sender<'static, ThreadModeRawMutex, Rag, CHANNEL_CAPACITY>,
 ) {
     loop {
@@ -89,7 +105,7 @@ async fn flash_mode_task(
             // for normal run mode.
             let _permit = semaphore.acquire(1).await.unwrap();
 
-            while in_flash_mode.load(Ordering::Relaxed) {
+            while system_mode.load(Ordering::Relaxed) == SystemMode::Flash {
                 // Flash On Phase
                 rags.send(Rag::new(Leg::A, false, true, false)).await;
                 rags.send(Rag::new(Leg::B, false, true, false)).await;
@@ -100,6 +116,11 @@ async fn flash_mode_task(
                 rags.send(Rag::new(Leg::B, false, false, false)).await;
                 Timer::after_millis(1_000).await;
             }
+
+            // Yield Phase
+            rags.send(Rag::new(Leg::A, false, true, false)).await;
+            rags.send(Rag::new(Leg::B, false, true, false)).await;
+            Timer::after_millis(3_000).await;
 
             // Clear Crossring Phase
             rags.send(Rag::new(Leg::A, true, false, false)).await;
@@ -115,24 +136,53 @@ async fn flash_mode_task(
 async fn flash_button_task(
     normal_mode_semaphore: &'static CrossingSemaphore,
     flash_mode_semaphore: &'static CrossingSemaphore,
-    in_flash_mode: &'static AtomicBool,
+    system_mode: &'static AtomicSystemMode,
     onboard_button_raw: &'static Signal<ThreadModeRawMutex, bool>,
 ) {
+    // As we start, we hold all the permits. This effectively blocks the traffic
+    // light tasks from running, as they will be waiting for a permit to become
+    // available. Permits are represented as boolean values, since we can only
+    // ever have or have not one.
+    let mut have_normal_permit: bool = true;
+    let mut have_flash_permit: bool = true;
+
     loop {
-        onboard_button_raw.wait().await;
+        // At the program start, or after the mode is switched, we have to
+        // re-juggle the permits so that we block and unblock the right tasks.
+        // In the process of doing so, we have to be careful not to lose any
+        // permits (which is why they are represented as boolean values), or to
+        // allow tasks to overlap. To avoid overlap, we first try to collect any
+        // missing permits before releasing anything.
 
-        in_flash_mode.fetch_not(Ordering::Relaxed);
-
-        if in_flash_mode.load(Ordering::Relaxed) {
-            normal_mode_semaphore.acquire(1).await.unwrap().disarm();
-            flash_mode_semaphore.release(1);
-        } else {
-            flash_mode_semaphore.acquire(1).await.unwrap().disarm();
-            normal_mode_semaphore.release(1);
+        match system_mode.load(Ordering::Relaxed) {
+            SystemMode::Normal => {
+                ensure_aquired(&mut have_flash_permit, flash_mode_semaphore).await;
+                ensure_released(&mut have_normal_permit, normal_mode_semaphore);
+            }
+            SystemMode::Flash => {
+                ensure_aquired(&mut have_normal_permit, normal_mode_semaphore).await;
+                ensure_released(&mut have_flash_permit, flash_mode_semaphore);
+            }
         }
 
-        // debounce....
+        // Ignore events for a bit in order to debounce, before we await the
+        // user pressing the on-board pushbutton to switch to the next mode.
         Timer::after_millis(200).await;
+        onboard_button_raw.wait().await;
+        system_mode.to_next_mode();
+    }
+}
+
+async fn ensure_aquired(permit: &mut bool, semaphore: &'static CrossingSemaphore) {
+    if !*permit {
+        semaphore.acquire(1).await.unwrap().disarm();
+        *permit = true;
+    }
+}
+fn ensure_released(permit: &mut bool, semaphore: &'static CrossingSemaphore) {
+    if *permit {
+        semaphore.release(1);
+        *permit = false;
     }
 }
 
@@ -162,7 +212,7 @@ async fn main(spawner: Spawner) {
     spawner
         .spawn(flash_mode_task(
             &FLASH_MODE_SEMAPHORE,
-            &IN_FLASH_MODE,
+            &SYSTEM_MODE,
             RAGS.sender(),
         ))
         .unwrap();
@@ -170,26 +220,33 @@ async fn main(spawner: Spawner) {
         .spawn(flash_button_task(
             &NORMAL_MODE_SEMAPHORE,
             &FLASH_MODE_SEMAPHORE,
-            &IN_FLASH_MODE,
+            &SYSTEM_MODE,
             &ONBOARD_BUTTON_RAW,
         ))
         .unwrap();
 
-    // Show and help count seconds by flashing the on-board LED roughly once
-    // every second. In normal mode we just flash, in maintenance mode we blink
-    // on an off.
+    // Help count seconds by flashing the on-board LED roughly once every
+    // second. In normal mode we just flash, in maintenance mode we blink on and
+    // off.
     loop {
         BLINKY.send(true).await;
-        if IN_FLASH_MODE.load(Ordering::Relaxed) {
-            Timer::after_millis(500).await;
-        } else {
-            Timer::after_millis(15).await;
+        match SYSTEM_MODE.load(Ordering::Relaxed) {
+            SystemMode::Normal => {
+                Timer::after_millis(15).await;
+            }
+            SystemMode::Flash => {
+                Timer::after_millis(500).await;
+            }
         }
+
         BLINKY.send(false).await;
-        if IN_FLASH_MODE.load(Ordering::Relaxed) {
-            Timer::after_millis(500).await;
-        } else {
-            Timer::after_millis(985).await;
+        match SYSTEM_MODE.load(Ordering::Relaxed) {
+            SystemMode::Normal => {
+                Timer::after_millis(985).await;
+            }
+            SystemMode::Flash => {
+                Timer::after_millis(500).await;
+            }
         }
     }
 }
